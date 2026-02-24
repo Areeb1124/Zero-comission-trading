13b875101bef78397271def433b51877ad8d0252
Areeb1124 <Areeb81ahmed@gmail.com>
2026-02-24 16:00:12 +0530

    feat: navbar.footer and open Accoutn UI

--- src/landing_page/Navbar.jsx
+++ src/landing_page/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
-    return (
-        <h1>Navbar</h1>
-      );
+  return (
+    <nav class="navbar navbar-expand-lg border-bottom " style={{ backgroundColor: "#FFF" }}>
+      <div class="container p-2">
+        <a class="navbar-brand" href="#">
+          <img src="Media\Images\logo (1).svg" alt="Logo" style={{width:"25%"}}/>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link " aria-current="page" href="#">
+              Signup
+            </a>
+            <a class="nav-link" href="#">
+              About
+            </a>
+            <a class="nav-link" href="#">
+              Product
+            </a>
+            <a class="nav-link" aria-disabled="true">
+              Pricing 
+            </a>
+             <a class="nav-link" aria-disabled="true">
+              Support 
+            </a>
+             <a class="nav-link" aria-disabled="true">
+               <i class="fa-solid fa-bars"></i>
+            </a> 
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
