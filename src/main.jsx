c3192839c98ff999610b06f5170361e6cade155f
Areeb1124 <Areeb81ahmed@gmail.com>
2026-02-26 22:53:48 +0530

    Working on the About component

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 // import App from './App.jsx'
 import HomePage from './landing_page/home/HomePage.jsx';
 // import Signup from "./landing_page/signup/SignUp.jsx";
-// import AboutPage from "./landing_page/about/AboutPage.jsx";
+import AboutPage from "./landing_page/about/AboutPage.jsx";
 // import ProductPage from "./landing_page/products/ProductsPage.jsx";
 // import PricingPage from "./landing_page/pricing/PricingPage.jsx";
 // import SupportPage from "./landing_page/support/SupportPage.jsx";
@@ -18,9 +18,9 @@ createRoot(document.getElementById('root')).render(
   <Navbar/>
   <Routes>
     <Route path="/" element={<HomePage />} />
-    {/* <Route path="/signup" element={<Signup />} />
+    {/* <Route path="/signup" element={<Signup />} /> */}
     <Route path="/about" element={<AboutPage />} />
-    <Route path="/product" element={<ProductPage />} />
+    {/* <Route path="/product" element={<ProductPage />} />
     <Route path="/pricing" element={<PricingPage />} />
     <Route path="/support" element={<SupportPage />} /> */}
     <Route path='*' element={<NotFound/>}/>
