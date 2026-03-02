7296f783387c2509cf4f4578c2e21e12cfd8460a
Areeb1124 <Areeb81ahmed@gmail.com>
2026-03-03 01:14:09 +0530

    Working on the Product component

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import HomePage from './landing_page/home/HomePage.jsx';
 // import Signup from "./landing_page/signup/SignUp.jsx";
 import AboutPage from "./landing_page/about/AboutPage.jsx";
-// import ProductPage from "./landing_page/products/ProductsPage.jsx";
+import ProductPage from "./landing_page/products/ProductsPage.jsx";
 // import PricingPage from "./landing_page/pricing/PricingPage.jsx";
 // import SupportPage from "./landing_page/support/SupportPage.jsx";
 import Navbar from './landing_page/Navbar.jsx';
@@ -20,8 +20,8 @@ createRoot(document.getElementById('root')).render(
     <Route path="/" element={<HomePage />} />
     {/* <Route path="/signup" element={<Signup />} /> */}
     <Route path="/about" element={<AboutPage />} />
-    {/* <Route path="/product" element={<ProductPage />} />
-    <Route path="/pricing" element={<PricingPage />} />
+    <Route path="/product" element={<ProductPage />} />
+    {/* <Route path="/pricing" element={<PricingPage />} />
     <Route path="/support" element={<SupportPage />} /> */}
     <Route path='*' element={<NotFound/>}/>
   </Routes>
