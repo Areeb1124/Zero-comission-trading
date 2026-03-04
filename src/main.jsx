273224c0f4838d1ec596774018188c7ad4834120
Areeb1124 <Areeb81ahmed@gmail.com>
2026-03-05 02:01:20 +0530

    Working on the Pricing component

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import HomePage from './landing_page/home/HomePage.jsx';
 // import Signup from "./landing_page/signup/SignUp.jsx";
 import AboutPage from "./landing_page/about/AboutPage.jsx";
 import ProductPage from "./landing_page/products/ProductsPage.jsx";
-// import PricingPage from "./landing_page/pricing/PricingPage.jsx";
+import PricingPage from "./landing_page/pricing/PricingPage.jsx";
 // import SupportPage from "./landing_page/support/SupportPage.jsx";
 import Navbar from './landing_page/Navbar.jsx';
 import Footer from './landing_page/Footer.jsx';
@@ -15,14 +15,13 @@ import NotFound from './landing_page/NotFound.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <Navbar/>
   <Routes>
-    <Route path="/" element={<HomePage />} />
+    <Route path="/" element={<><Navbar/><HomePage /></>} />
     {/* <Route path="/signup" element={<Signup />} /> */}
-    <Route path="/about" element={<AboutPage />} />
-    <Route path="/product" element={<ProductPage />} />
-    {/* <Route path="/pricing" element={<PricingPage />} />
-    <Route path="/support" element={<SupportPage />} /> */}
+    <Route path="/about" element={<><Navbar/><AboutPage /></>} />
+    <Route path="/product" element={<><Navbar/><ProductPage /></>} />
+    <Route path="/pricing" element={<><nav className="pricing-navbar-fixed"><Navbar/></nav><PricingPage /></>} />
+    {/* <Route path="/support" element={<SupportPage />} /> */}
     <Route path='*' element={<NotFound/>}/>
   </Routes>
   <Footer/>
